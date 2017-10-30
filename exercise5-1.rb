38ca0da1dbdddd6d99230355c736ffc0568cbaf0
Eric Marrone <emarrone17@gmail.com>
2017-10-30 16:59:38 -0400

    Add exercise 5.1

---
+++ exercise5-1.rb
@@ -0,0 +1,18 @@
+counter = 0
+
+while counter < 100
+  puts  "Would you like to walk or run?"
+  user_input = gets.chomp.to_s
+  if user_input == "walk"
+    counter += 1
+    puts "Distance from home is #{counter}km"
+  elsif user_input == "run"
+    counter += 5
+    puts "Distance from home is #{counter}km"
+  elsif user_input == "go home"
+    puts "Workout done. Time for home"
+    break
+  else
+    puts "Invalid command. Input 'walk' or 'run'."
+  end
+end

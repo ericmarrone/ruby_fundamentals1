fca9ff4702637e32dcd33f72e2d2057f6d3a5776
Eric Marrone <emarrone17@gmail.com>
2017-10-30 15:26:45 -0400

    Add exercise 4.3

---
+++ exercise4-3.rb
@@ -0,0 +1,9 @@
+my_name = "Eric"
+puts "What is your name?"
+user_name = gets.chomp.to_s
+
+if user_name == my_name
+  puts "We have the same name!"
+else puts "Nice to meet you #{user_name}!"
+
+end

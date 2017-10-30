00269978bee28a0ec177c41240300a89c7573c42
Eric Marrone <emarrone17@gmail.com>
2017-10-30 14:53:29 -0400

    Add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,11 @@
+# Question 1
+puts 55 * 0.15
+
+# Question 2
+puts "2".to_i + 2
+
+# Question 3
+puts "This is a number: #{45628 * 7389}."
+
+# Question 4
+puts (10 < 20 && 30 < 20)  || !(10 == 11)
